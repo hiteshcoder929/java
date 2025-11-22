da5879460bc7e0a4b4139bb8261ff95365009859
hiteshcoder929 <hiteshcoder929.noreply.github.com>
2025-11-22 23:02:52 +0530

    jaba

---
+++ code.js
@@ -0,0 +1,14 @@
+let anime = ["dragonBall", "naruto", "onePiece", "bleach", "myHeroAcademia"];
+
+const mC =[
+    {Name: "goku"},
+    {Name: "naruto"},
+    {Name: "luffy"},
+    {Name: "ichigo"},
+    {Name: "deku"}
+];
+
+
+for(let i=0; i<anime.length; i++ ){
+console.log(anime[i] + " MC " + mC[i].Name);
+}
